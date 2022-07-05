831fd684a58224fcdf93f39d0d045e5bb7fcf1a8
Eugene Helland <eugenehelland@mail.com>
2022-07-05 14:33:32 -0600

    Undo redundant change to views/layouts/application.html.erb

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,30 +1,9 @@
 class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
-
+  
   def show
     @product_count = Product.all.count
     @category_count = Category.all.count
-    
-    @categories = Category.all
   end
-
-  def new
-    @category = Category.new
-  end
-
-  def create
-    @category = Category.new(category_name)
-
-    if @category.save
-      redirect_to [:admin], notice: 'Category Created!'
-    end
-  end
-
-  def category_name
-    params.require(:category).permit(
-      :name
-    )
-  end
-
 end
