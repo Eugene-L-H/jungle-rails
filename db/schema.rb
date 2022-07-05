d60cc187bb916ab150745f8c055d8534e5b4d3e3
Eugene Helland <eugenehelland@mail.com>
2022-07-05 17:45:20 -0600

    Some changes due to troubleshooting database issue, add user validation tests.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_30_143531) do
+ActiveRecord::Schema.define(version: 2022_07_05_204728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,9 +54,6 @@ ActiveRecord::Schema.define(version: 2022_06_30_143531) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
