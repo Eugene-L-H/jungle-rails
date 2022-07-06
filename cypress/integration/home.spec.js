1d11db4e00f41a435b790e2f5be5a1f618db0f67
Eugene Helland <eugenehelland@mail.com>
2022-07-05 18:39:11 -0600

    Add testing for navigation from home to product page.

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,8 +1,22 @@
-it("There is products on the page", () => {
-  cy.visit("/products");
-  cy.get(".products article").should("be.visible");
-});
+/// <reference types="cypress" />
 
-it("There is 2 products on the page", () => {
-  cy.get(".products article").should("have.length", 2);
+describe("testing main page products", () => {
+  beforeEach(() => {
+    // Visits home page before every test
+    cy.visit("/");
+  });
+  /////////////////////////////////////////////////////////////
+  // it('should display the list of products on the main page', () => {
+  //   // checks if container exists
+  //   cy.get('.products').should('exist')
+  //   cy.get('#products-list').should('exist')
+  // })
+  /////////////////////////////////////////////////////////////
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+  /////////////////////////////////////////////////////////////
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
 });
